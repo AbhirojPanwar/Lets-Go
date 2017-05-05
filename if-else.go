2c00298845239c608efb04e937030eaeea2338c4
Abhiroj Panwar <abhiroj96@gmail.com>
2017-05-05 22:53:40 +0530

    switch and function closure

---
+++ if-else.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+if 8%2!=0 {
+	fmt.Println("Number is odd")
+} else {
+	fmt.Println("Number is even")
+}
+
+if n:=2; n<3 {
+	fmt.Println("Printing n",n)
+}
+
+
+}
---
+++ switch.go
@@ -0,0 +1,54 @@
+package main 
+
+import "fmt"
+import "time"
+
+func main(){
+
+i:=2
+fmt.Print("Write ",i," as ")
+switch i{
+case 1:
+	fmt.Println("one")
+case 2:
+	fmt.Println("two")
+case 3:
+	fmt.Println("three")
+}
+
+weekday:=time.Now().Weekday()
+
+switch weekday {
+case time.Saturday,time.Sunday:
+	fmt.Println("It is a weekend")
+default:
+	fmt.Println("It is a weekday")
+}
+
+t := time.Now()
+
+switch{
+case t.Hour()<12:
+	fmt.Println("It is before noon")
+default:
+	fmt.Println("It is after noon")
+
+}
+
+// Function Closure
+whatAmI := func(i interface{}){
+	switch t := i.(type){
+		case bool:
+            fmt.Println("I'm a bool")
+        case int:
+            fmt.Println("I'm an int")
+        default:
+            fmt.Printf("Don't know type %T\n", t)
+	}
+}
+
+whatAmI(true)
+whatAmI(1)
+whatAmI("string")
+
+}
